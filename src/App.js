474a3013dadc72e132403f1f09a4b9b5c758f3fd
Holtzen <mystikaljester@gmail.com>
2018-01-18 20:01:41 -0600

    Skeleton for list completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">React Todo List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="todo container">
+          <div className="todo addnew">
+            <input type="text" id='newtodo' placeholder="Add new todo"/>
+          </div>
+          <ul className="todo list">
+            <li className="todo item">
+              do stuff
+            </li>
+            <li className="todo item">
+              do other stuff
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
